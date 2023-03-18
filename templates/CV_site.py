376636b2bd55dba60f6ef3b27ed46b21948f9568
Lawrence Magee <lawrence.magee@outlook.com>
2023-03-18 16:57:11 +0800

    Update CV_site.py
    refactor code with flask_frozen

--- templates/CV_site.py
+++ templates/CV_site.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
-
+freezer = Freezer(app)
 
 @app.route('/index/')
 def home():
     return render_template("index.html")
-
 
 @app.route('/about/')
 def about():
     return render_template("about.html")
 
 if __name__=="__main__":
-    app.run(debug= True)
+    freezer.freeze()
